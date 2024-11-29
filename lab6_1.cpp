98b5d40a492fe4b163b5774237e0bfe18a705295
Phenomenon <xiii15499@gmail.com.com>
2024-11-29 16:06:20 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int i,odd=0,even=0;
+
+    while(i!=0){
+      cout << "Enter an integer: ";
+      cin>>i;
+      if(i!=0){
+            if(i%2==0){
+                even++;
+            }else{
+                odd++;
+            }
+        }
+    }
+    cout << "#Even numbers = "<<even<<"\n";
+    cout << "#Odd numbers = "<<odd;
     return 0;
 }
